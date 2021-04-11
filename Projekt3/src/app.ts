4e52b9da9e89fb68dc21347a2957115f8c0de1ae
TheTomson <tomekdrom@interia.pl>
2021-04-11 13:38:06 +0200

    StarterWeather v1

---
+++ Projekt3/src/app.ts
@@ -0,0 +1,28 @@
+export class App {
+    opwApiKey = '6c1a04409b8b3c406d3c24d219370557';
+    constructor() {
+        this.getCityInfo('warszawa')
+    }
+    async getCityInfo(city: string) {
+        const weather = await this.getWeather('warszawa');
+        this.saveData(weather);
+    }
+    async getWeather(city: string): Promise<any> {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        return weatherData;
+    }
+    saveData(data: any) {
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    }
+    getData() {
+        const data = localStorage.getItem('weatherData');
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return {};
+        }
+    }
+}
