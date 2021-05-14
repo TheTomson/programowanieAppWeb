948db5a93f41ec2e462f625e26ebb58f22c7e9d7
TheTomson <tomekdrom@interia.pl>
2021-05-14 19:22:49 +0200

    Update without images

--- Projekt3/src/app.ts
+++ Projekt3/src/app.ts
@@ -1,18 +1,45 @@
 export class App {
     opwApiKey = '6c1a04409b8b3c406d3c24d219370557';
+    btn: HTMLButtonElement;
+    city: string;
+    cardsContainer: HTMLDivElement;
     constructor() {
-        this.getCityInfo('warszawa')
+        this.getInput();
+        this.addListener();
+        this.getCityInfo();
     }
-    async getCityInfo(city: string) {
-        const weather = await this.getWeather('warszawa');
+    getInput = () =>{
+    this.btn = document.querySelector('button');
+    this.cardsContainer = document.querySelector('#ShowCity');
+    }
+    addListener = () =>{
+        this.btn.addEventListener('click', () => this.getCityInfo());
+        }
+    async getCityInfo() {
+        this.city = document.querySelector('input').value;
+        document.querySelector('input').value = '';
+        const weather = await this.getWeather();
         this.saveData(weather);
     }
-    async getWeather(city: string): Promise<any> {
-        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+    Celcius(val: number){
+        return val - 273.15;
+    }  
+    async getWeather() {
+        let getElem = document.querySelector("#ShowCity");
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.opwApiKey}`;
         const weatherResponse = await fetch(openWeatherUrl);
         const weatherData = await weatherResponse.json();
         console.log(weatherData);
-        return weatherData;
+        let WeatherDiv= `<div class="weatherInfo"><h2>`+this.city+`</h2> 
+        <h4>{`+weatherData.coord.lon+`,`+weatherData.coord.lat+`}</h4>
+        <br/>Weather:
+        <br/>Temp{`+this.Celcius(weatherData.main.temp).toString().substr(0,5)+`C ,`+weatherData.main.temp+`K }
+        <br/>Clouds: `+weatherData.clouds.all+`%`+`
+        <br/>Wind{speed: `+weatherData.wind.speed+`, degree:`+weatherData.wind.deg+`}
+        <br/>Visibility: `+weatherData.visibility.toString().substr(0,3)+`%`+` 
+        <br/>Pressure: `+weatherData.main.pressure+` hPa
+        `;
+        getElem.innerHTML += WeatherDiv;
     }
     saveData(data: any) {
         localStorage.setItem('weatherData', JSON.stringify(data));
@@ -25,4 +52,5 @@ export class App {
             return {};
         }
     }
+  
 }
